35fc738ff129f6125c59bf638c424dd30169f2e9
liquid0624 <rjbirnesser@gmail.com>
2018-01-06 16:37:41 -0500

    msm8998: import oneplus 3 doze package
    Credit: DirtyUnicorns

--- device.mk
+++ device.mk
@@ -155,7 +155,13 @@ PRODUCT_COPY_FILES += \
 
 # Doze
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    OneplusThreeDoze
+
+# DRM
+PRODUCT_PACKAGES += \
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service \
+    android.hardware.drm@1.0-service.widevine
 
 # Wifi
 PRODUCT_PACKAGES += \
