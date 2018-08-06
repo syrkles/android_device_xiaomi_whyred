0981a3f2cd943601e9ac2ca5d508a688b7665a03
syrkles <syrkles@gmail.com>
2018-08-06 22:33:18 +0000

    Revert "get invictrix building"
    This reverts commit 7718017037dc078495619fc623944fc06ae7be21.

--- device.mk
+++ device.mk
@@ -112,23 +112,15 @@ PRODUCT_COPY_FILES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.allocator@2.0-impl \
-    android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.composer@2.1-impl \
-    android.hardware.graphics.composer@2.1-service \
-    android.hardware.graphics.mapper@2.0-impl \
-    android.hardware.memtrack@1.0-impl \
-    android.hardware.memtrack@1.0-service \
-    copybit.msm8998 \
-    gralloc.msm8998 \
-    hwcomposer.msm8998 \
-    memtrack.msm8998 \
-    libdisplayconfig \
-    libgenlock \
+    copybit.sdm660 \
+    gralloc.sdm660 \
+    hwcomposer.sdm660 \
+    memtrack.sdm660 \
     liboverlay \
+    libdisplayconfig \
+    libhwc2on1apadapter \
     libqdMetaData.system \
     libtinyxml
-    
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs.xml \
